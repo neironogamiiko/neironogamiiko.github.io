a837afdd87b0b07fc282bdfbf3ddaa26ad69cdf2
neironogamiiko <neironogamiiko@gmail.com>
2020-04-27 16:41:42 +0300

    site commit

--- script.js
+++ script.js
@@ -7,13 +7,7 @@ $(document).ready(function() {
 			type: "POST",
 			url: "mail.php", //Change
 			data: th.serialize()
-		}).done(function() {
-			alert("Thank you!");
-			setTimeout(function() {
-				// Done Functions
-				th.trigger("reset");
-			}, 1000);
-		});
+		})
 		return false;
 	});
 
